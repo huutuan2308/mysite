787f445acefe1338e331ecc6fad918071f423869
HuuTuan <huutuan238@gmail.com>
2020-08-05 13:32:06 +0700

    part4-part7

--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,3 @@
-# from django.urls import path
-# from django.conf.urls import url
-
-# from . import views
-
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('<int:question_id>/', views.detail, name='detail'),
-#     # url(r'^details/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-#     path('<int:question_id>/results/', views.results, name='results'),
-#     # path('<int:question_id>/vote/', views.vote, name='vote'),
-#     path('<int:question_id>/vote/', views.vote, name='vote'),
-# ]
 from django.urls import path
 
 from . import views
